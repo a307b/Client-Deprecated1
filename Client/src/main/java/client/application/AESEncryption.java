8f93887b47f6d22eab9a3371b39bacfc8af762a1
kris914g <Kristoffer@aagard.dk>
2018-04-04 14:32:38 +0200

    Added Code

---
+++ Client/src/main/java/client/application/AESEncryption.java
@@ -0,0 +1,55 @@
+package client.application;
+
+import java.security.AlgorithmParameters;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class AESEncryption {
+
+    public String encrypt(String word) throws Exception {
+        byte[] ivBytes;
+        String password="2b6%u3-JEiL}V'mXzQVKh1j=JY9Oo%ds;<nF(MAznrrtDI-L6Jvy3!|D2?pS.MV"; /*you can give whatever you want for password. This is for testing purpose*/
+
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[20];
+        random.nextBytes(bytes);
+        byte[] saltBytes = bytes;
+
+        //Derive the key
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, 65556, 256);
+
+        SecretKey secretKey = factory.generateSecret(spec);
+        SecretKeySpec secret = new SecretKeySpec(secretKey.getEncoded(), "AES");
+
+        //Encryption the word
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        AlgorithmParameters params = cipher.getParameters();
+        ivBytes = params.getParameterSpec(IvParameterSpec.class).getIV();
+        byte[] encryptedTextBytes = cipher.doFinal(word.getBytes("UTF-8"));
+
+        //Prepend salt and VI
+
+        byte[] buffer = new byte[saltBytes.length + ivBytes.length + encryptedTextBytes.length];
+
+        System.arraycopy(saltBytes, 0, buffer, 0, saltBytes.length);
+        System.arraycopy(ivBytes, 0, buffer, saltBytes.length, ivBytes.length);
+
+        System.arraycopy(encryptedTextBytes, 0, buffer, saltBytes.length + ivBytes.length, encryptedTextBytes.length);
+
+        return new Base64().encodeToString(buffer);
+
+    }
+}
+
---
+++ Client/src/main/java/client/application/RSAEksempel.java
@@ -0,0 +1,51 @@
+package client.application;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import javax.crypto.Cipher;
+
+public class RSAEksempel {
+    public static void main(String [] args) throws Exception {
+        JSONObject obj =  new JSONObject();
+        obj.put("Hello", "World");
+        obj.put("Lets", "Dance");
+
+        // Get an instance of the RSA key generator
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+
+        // Generate the keys
+        KeyPair myPair = kpg.generateKeyPair();
+
+        // Get an instance of the Cipher for RSA encryption/decryption
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+
+        // Initiate the Cipher, telling it that it is going to Encrypt, giving it the public key
+        cipher.init(Cipher.ENCRYPT_MODE, myPair.getPublic());
+        // Create a secret message
+        byte[] text = obj.toString().getBytes("UTF8");
+        // Cipher it
+        byte[] textEncrypted = cipher.doFinal(text);
+        // Write the message to an data file
+        Path filePath = Paths.get("C:\\Journal\\RSAEncrypted.dat");
+        Files.write(filePath, textEncrypted);
+
+        // Reads the data file and saves it in fileContent.
+        byte[] fileContent =  Files.readAllBytes(filePath);
+        // Initiate the decipher
+        cipher.init(cipher.DECRYPT_MODE, myPair.getPrivate());
+        // Decipher the message
+        byte[] textDecrypted = cipher.doFinal(fileContent);
+        // Converts the bytes to a JSON objects
+        JSONParser parser = new JSONParser();
+        JSONObject returnedJsonObj = (JSONObject) parser.parse(new String(textDecrypted));
+
+        // Prints the JSON Object
+        System.out.println(returnedJsonObj);
+    }
+}
